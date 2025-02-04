18af387fbbd3403a6b56e616f1106a4477bdd023
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-02-05 00:34:36 +0900

    fix: 빌드 성공

--- src/test/java/org/example/honorsparkingbe/unit/service/ParkingInfoServiceTest.java
+++ src/test/java/org/example/honorsparkingbe/unit/service/ParkingInfoServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -32,7 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ParkingInfoServiceTest {
 
     @Mock
