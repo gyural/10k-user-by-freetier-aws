49091813797c1e3e79095707bb979ab148de5bcd
donggeun <dgsim126@gmail.com>
2025-02-10 18:54:40 +0900

    fix: add session info(id)

--- src/main/java/org/example/honorsparkingbe/controller/SessionController.java
+++ src/main/java/org/example/honorsparkingbe/controller/SessionController.java
@@ -19,15 +19,8 @@ public class SessionController {
         // 현재 인증 정보 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // `CustomUserDetails`로 캐스팅하여 memberId 가져오기 !수정!
-        Long memberId = null; // !수정!
-        if (authentication.getPrincipal() instanceof CustomUserDetails) { // !수정!
-            memberId = ((CustomUserDetails) authentication.getPrincipal()).getId(); // !수정!
-        }
-
         // 응답 데이터 생성
         Map<String, Object> response = new HashMap<>();
-        response.put("memberId", memberId);  // 🔥 memberId 추가 !수정!
         response.put("userName", authentication.getName());
         response.put("authorities", authentication.getAuthorities());
         response.put("authenticated", authentication.isAuthenticated());
