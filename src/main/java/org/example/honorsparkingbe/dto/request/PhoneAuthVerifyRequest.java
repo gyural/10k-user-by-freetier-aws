e6c9dde2fa89c345831792949241c780b6d7a423
Shihyun Han <devowl99@gmail.com>
2025-03-27 03:11:49 +0900

    ref: Add annotations and for saving

--- src/main/java/org/example/honorsparkingbe/dto/request/PhoneAuthVerifyRequest.java
+++ src/main/java/org/example/honorsparkingbe/dto/request/PhoneAuthVerifyRequest.java
@@ -13,3 +13,7 @@ public class PhoneAuthVerifyRequest {
     private String authCode;
 
 }
+
+// 인증번호(authCode) 를 Redis에 저장할 때 "phoneauth:{phoneNumber}" 라는 전화번호 기반 키로 저장했기 때문에,
+// 검증 시에도 어떤 전화번호 기준으로 인증번호를 꺼낼지 알아야 한다.
+// 그러므로 authCode 만이 아닌 phoneNumber도 필요한 것.
