0d956d3a3e84e84867471b43a4721d10679d5aa1
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-04-02 16:23:52 +0900

    fix: 환경변수 문제 해결

--- src/test/java/org/example/honorsparkingbe/unit/util/ApiKeyAuthFilterUnitTest.java
+++ src/test/java/org/example/honorsparkingbe/unit/util/ApiKeyAuthFilterUnitTest.java
@@ -1,5 +1,6 @@
 package org.example.honorsparkingbe.unit.util;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,18 +13,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = "api.key=valid-api-key")
 public class ApiKeyAuthFilterUnitTest {
 
   @Autowired
   private MockMvc mockMvc;
   @Autowired
   private ObjectMapper objectMapper;
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("api.key", () -> "valid-api-key");
+  }
 
   @Test
   void API_KEY가_정상적으로_인증되면_200_OK() throws Exception {
@@ -32,7 +38,7 @@ public class ApiKeyAuthFilterUnitTest {
     mockMvc.perform(post("/api/v1/sync/inout")
             .header("X-API-KEY", "valid-api-key")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(requestBody)
+            .content(requestBody).with(csrf())
         )
         .andExpect(status().isOk());
   }
