49a151717e21d91b812241c4dcfa67eefd0fd49f
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-04-17 15:40:59 +0900

    feat: 스케줄러 활성화

--- src/main/java/org/example/honorsparkingbe/HonorsParkingBeApplication.java
+++ src/main/java/org/example/honorsparkingbe/HonorsParkingBeApplication.java
@@ -2,9 +2,10 @@ package org.example.honorsparkingbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class HonorsParkingBeApplication {
 
   public static void main(String[] args) {
