5da87862cec074a0b098baa65cb6a0fb3bf4e35f
donggeun <dgsim126@gmail.com>
2025-05-11 01:12:57 +0900

    before

--- src/main/java/org/example/honorsparkingbe/security/CustomOAuth2LoginSuccessHandler.java
+++ src/main/java/org/example/honorsparkingbe/security/CustomOAuth2LoginSuccessHandler.java
@@ -17,15 +17,15 @@ public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHan
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         // 로그인 성공 후 프론트엔드로 리디렉트
-        // response.sendRedirect("http://localhost:3000/");
+        response.sendRedirect("http://localhost:3000/");
         // response.sendRedirect("https://honorsparking-web.vercel.app/");
-        CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
-        String role= user.getAuthorities().iterator().next().getAuthority();
-
-        if("ROLE_USER".equals(role)){
-            response.sendRedirect("https://honorsparking-web.vercel.app/");
-        }else{
-            response.sendRedirect("https://honorsparking-web.vercel.app/hello");
-        }
+//        CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
+//        String role= user.getAuthorities().iterator().next().getAuthority();
+//
+//        if("ROLE_USER".equals(role)){
+//            response.sendRedirect("https://honorsparking-web.vercel.app/");
+//        }else{
+//            response.sendRedirect("https://honorsparking-web.vercel.app/hello");
+//        }
     }
 }
