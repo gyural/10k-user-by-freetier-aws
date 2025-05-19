1225855edf38668e62bdc967263088a3fb05430a
Shihyun Han <devowl99@gmail.com>
2025-05-19 20:08:53 +0900

    feat: SyncInoutResponse restoration

--- src/main/java/org/example/honorsparkingbe/dto/response/SyncInoutResponse.java
+++ src/main/java/org/example/honorsparkingbe/dto/response/SyncInoutResponse.java
@@ -1,6 +1,5 @@
 package org.example.honorsparkingbe.dto.response;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,8 +10,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class SyncInoutResponse {
 
   List<ParkingEntry> ValidNonExitEntries;
@@ -25,14 +22,5 @@ public class SyncInoutResponse {
   public static class ParkingEntry {
 
     private Long id;
-
-    // 비회원 차량 조회를 위해 필요한 필드
-    // 응답 타입 유사하므로 기존의 dto 확장하여 활용. 이후 분리 가능
-    private String vehicleNumber;
-    private String parkingLotLocation;
-    private LocalDateTime entryTime;
-    private Integer totalParkingMinutes;
-    private Integer currentFee;
-    private String entryPhotoUrl;
   }
 }
